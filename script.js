a0854d52ff1ed1bada167117e6d96dfe7d473b83
Stephan Griesel <sgriesel@gmail.com>
2018-04-15 09:25:35 +0200

    image slider with lory js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    run();
+});
+
+function run() {
+    var slider = document.querySelector('.js-carousel');
+    lory(slider, {
+        infinite:1,
+        enableMouseEvents: true,
+        classNameFrame: 'js-carousel__frame',
+        classNameSlideContainer: 'js-carousel__slides',
+        classNamePrevCtrl: 'js-carousel__prev',
+        classNameNextCtrl: 'js-carousel_next',
+    })
+}
